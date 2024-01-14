de5c5d99721243806c52576305060a32ac88281e
skyth540 <g.day540@gmail.com>
2024-01-13 18:32:07 -0700

    Finsihed 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage?");
+        string answer = Console.ReadLine();
+        int percent = int.Parse(answer);
+
+        string letterg = "";
+
+        if (percent >= 90)
+        {
+            letterg = "A";
+        }
+        else if (percent >= 80)
+        {
+            letterg = "B";
+        }
+        else if (percent >= 70)
+        {
+            letterg = "C";
+        }
+        else if (percent >= 60)
+        {
+            letterg = "D";
+        }
+        else
+        {
+            letterg = "F";
+        }
+
+        Console.WriteLine($"Your grade is: {letterg}");
+
+        if (percent >= 70)
+        {
+            Console.WriteLine("You passed");
+        }
+        else
+        {
+            Console.WriteLine("You will have to re-take");
+        }
     }
 }
